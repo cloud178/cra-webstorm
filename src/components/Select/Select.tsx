ff008f987641642ab39f7d005fe5b81bac8896a2
Denis Biryukov <denis-biryukov-13@mail.ru>
2025-02-01 00:47:20 +0300

    18 react HW

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -14,11 +14,10 @@ type SelectPropsType = {
 };
 
 const SelectSecret = (props: SelectPropsType) => {
-
+// export const Select = (props: SelectPropsType) => {
+    console.log('select rendering');
     const [active, setActive] = useState(false);
     const [hoveredElementValue, setHoveredElementValue] = useState(props.value);
-    const InputRef = React.useRef<HTMLInputElement>(null);
-    const [population, setPopulation] = useState<number>(0);
 
     const selectedItem = props.items.find(i => i.value === props.value);
     const hoveredItem = props.items.find(i => i.value === hoveredElementValue);
@@ -53,58 +52,31 @@ const SelectSecret = (props: SelectPropsType) => {
                 props.onChange(props.items[0].value)
             }
         }
-
         if (e.key === 'Enter' || e.key === 'Escape') {
             setActive(false);
         }
     }
 
-    const filteredByPopuolationCities = useMemo(() => {
-        console.log(999)
-        let i = 0
-        while (i < 500000000) {
-            i++
-            Math.random()
-        }
-        return props.items.filter(i => i.population ? i.population > population : null)
-    }, [population])
-
-
-
     return (
-        <div style={{display: 'flex', flexDirection: 'column', gap: '80px'}}>
-            <div className={s.select} tabIndex={0} onKeyUp={onKeyUpPres}>
-                <span className={s.main} onClick={toggleItems}>{selectedItem && selectedItem.title}</span>
-                {
-                    active &&
-                    <div className={s.items}>
-                        {props.items.map(i => {
-                                return (
-                                    <div
-                                        onMouseEnter={() => setHoveredElementValue(i.value)}
-                                        className={s.item + ' ' + (hoveredItem === i ? s.selected : '')}
-                                        key={i.value}
-                                        onClick={() => onItemClick(i.value)}
-                                    >{i.title}
-                                    </div>
-                                )
-                            }
-                        )}
-                    </div>
-                }
-            </div>
-            <div>
-                <input ref={InputRef} placeholder={'insert population filter'}/>
-                <button onClick={() => {
-                    if (InputRef.current) {
-                        setPopulation(+InputRef.current.value)
-                        InputRef.current.value = ""
-                    }
-                }}>+</button>
-                {filteredByPopuolationCities.map(i => (
-                    <p key={i.value}>{`${i.value} - ${i.title}, ${i.population ?? 'N/A'}`}</p>
-                ))}
-            </div>
+        <div className={s.select} tabIndex={0} onKeyUp={onKeyUpPres}>
+            <span className={s.main} onClick={toggleItems}>{selectedItem && selectedItem.title}</span>
+            {
+                active &&
+                <div className={s.items}>
+                    {props.items.map(i => {
+                            return (
+                                <div
+                                    onMouseEnter={() => setHoveredElementValue(i.value)}
+                                    className={s.item + ' ' + (hoveredItem === i ? s.selected : '')}
+                                    key={i.value}
+                                    onClick={() => onItemClick(i.value)}
+                                >{i.title}
+                                </div>
+                            )
+                        }
+                    )}
+                </div>
+            }
         </div>
     );
 };
--- src/components/Select/SelectMemoHW18.stories.tsx
+++ src/components/Select/SelectMemoHW18.stories.tsx
@@ -1,6 +1,6 @@
 import {ItemType, Select} from "./Select";
 import {action} from "@storybook/addon-actions";
-import {useState} from "react";
+import {memo, useMemo, useState} from "react";
 
 // todo нормально сделать домашку по инструкции
 
@@ -11,17 +11,44 @@ export default {
 
 export const DifferentSelects = () => {
     const [value, setValue] = useState('2')
+    let [counter, setCounter] = useState(0);
+
+    const items = [
+        {value: '1', title: 'Minsk', population: 1_800_000},
+        {value: '2', title: 'Amsterdam', population: 4_000_000},
+        {value: '3', title: 'LA', population: 7_000_000},
+    ]
+
+    const itemsWithLetterM = useMemo(() => {
+       return items.filter(i => i.title.toLowerCase().indexOf('m') !== -1)
+    }, [])
+
+    const itemsWithPopulationMoreThanTwoMillion = useMemo(() => {
+       return items.filter(i => i.population > 2_000_000)
+    }, [])
+
+    // const itemsWithLetterM = items.filter(i => i.title.toLowerCase().indexOf('m') !== -1)
+    //
+    //
+    // const itemsWithPopulationMoreThanTwoMillion = items.filter(i => i.population > 2_000_000)
+
+
 
     return (
-        <Select
-            value={value}
-            onChange={setValue}
-            items={[
-                {value: '1', title: 'Minsk', population: 1_800_000},
-                {value: '2', title: 'Amsterdam', population: 4_000_000},
-                {value: '3', title: 'LA', population: 7_000_000},
-            ]}
-        />
+        <>
+            <Select
+                value={value}
+                onChange={setValue}
+                items={itemsWithLetterM}
+            />
+            <Select
+                value={value}
+                onChange={setValue}
+                items={itemsWithPopulationMoreThanTwoMillion}
+            />
+            {counter}
+            <button onClick={() => setCounter(counter = counter + 1)}>increase counter</button>
+        </>
     )
 }
 
